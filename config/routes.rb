fccc45b4f68846120b7d33e399c791edd3f37296
Nagendranm5 <nagendransri3@gmail.com>
2022-02-20 00:28:38 +0530

    Routes Changed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources 'users'
-  get '/', to: 'users#users_list', as: 'users_list'
+  get 'users_list', to: 'users#users_list', as: 'users_list'
   resources :sessions, only: [:new, :create, :destroy]
   get 'delete_user', to: 'users#delete_user', as: 'delete_user'
   get 'signin', to: 'sessions#create', as: 'signin'
